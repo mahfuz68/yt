c847659c22611ffe557f9da4284a83d34411e19d
mahfuz68 <94157542+mahfuz68@users.noreply.github.com>
2022-01-25 18:22:20 +0600

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,25 +1,10 @@
-# importing packages
-from pytube import YouTube
-import os
+import pafy
 
-# url input from user
-yt = YouTube(
-	str(input("Enter the URL of the video you want to download: \n>> ")))
+url = "https://www.youtube.com/watch?v =eACohWVwTOc"
+video = pafy.new(url)
 
-# extract only audio
-video = yt.streams.filter(only_audio=True).first()
+audiostreams = video.audiostreams
+for i in audiostreams:
+	print(i.bitrate, i.extension, i.get_filesize())
 
-# check for destination to save file
-print("Enter the destination (leave blank for current directory)")
-destination = str(input(">> ")) or '.'
-
-# download the file
-out_file = video.download(output_path=destination)
-
-# save the file
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
-
-# result of success
-print(yt.title + " has been successfully downloaded.")
+audiostreams[3].download()
